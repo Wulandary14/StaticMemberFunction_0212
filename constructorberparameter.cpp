5a5a36470884dfe7ee287ce91007267951d298bd
Wulandary14 <Moonywulan14@gmail.com>
2026-05-04 18:49:02 +0700

    commit 12

--- constructorberparameter.cpp
+++ constructorberparameter.cpp
@@ -8,3 +8,13 @@ public:
 };
 
 //Definisi constructor dengan parameter
+mahasiswa::mahasiswa(int nim, string nama) {
+    cout << "Constructor Dengan Parameter Terpanggil" << endl;
+    cout << "NIM : " << nim << endl;
+    cout << "Nama : " << nama << endl;
+}
+
+int main() {
+    mahasiswa mhs(12345, "Shinoa");     //memanggil constructor deengan parameter 
+    return 0;
+}
