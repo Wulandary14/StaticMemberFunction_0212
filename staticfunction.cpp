87f3c54e8276ec11f566d55e6801af3d053885f2
Wulandary14 <Moonywulan14@gmail.com>
2026-05-04 18:33:06 +0700

    commit 7

--- staticfunction.cpp
+++ staticfunction.cpp
@@ -25,4 +25,19 @@ public:
     Mahasiswa(string pnama) :nama(pnama){
         setID();
     };
+};
+
+int Mahasiswa::nim = 0;
+
+//pendefinisial prosedur-prosedur diluar class
+void Mahasiswa::setID() {
+    id = ++nim;
 }
+
+void Mahasiswa::printAll(){
+    cout << "ID = " << id << endl;
+    cout << "Nama = " << nama << endl;
+    cout << endl;
+}
+
+int main() {}
